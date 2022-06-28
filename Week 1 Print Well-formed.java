62ccfb889e5aa9f129132d126b8b66fff56bb035
Ali810365 <mabdi@sea.yearup.org>
2022-06-28 14:42:16 -0700

    New

---
+++ Week 1 Print Well-formed.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+      System.out.println("A well-formed Java program has\na main method with { and }\nbraces. \r\n");
+      System.out.println("A System.out.println statement\nhas ( and ) and usually a\nString that starts and ends\nwith a \" character.\n(But we type \\\" instead!) \r\n ");
+      System.out.println("2. Take a screenshot of your code and the output and submit. \r\n");
+      System.out.println("Note: Due on Tue, Jul 5th due to Jul 4th Holiday on Monday.");
+    }
+  }
+  
