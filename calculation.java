cf84dbd9f106a4ceb8e5ab458c82b2dda491e00e
Ali810365 <mabdi@sea.yearup.org>
2022-07-10 19:36:14 -0700

    Week Two

---
+++ calculation.java
@@ -0,0 +1,13 @@
+public class calculation {
+    public static void main(String[] args) {
+      int valOne = 8;
+      int valTwo = 10;
+      int valThree = 5;
+      int valFour = 4;
+      int product = valOne * valTwo * valThree * valFour;
+      int average = (valOne + valTwo + valThree + valFour) / 4;
+      System.out.print(product + " " + average);
+      
+  
+    }
+  }
