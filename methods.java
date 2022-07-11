6375117d596b90b8f137e7766ae14696bb0687aa
Ali810365 <mabdi@sea.yearup.org>
2022-07-10 19:21:21 -0700

    Week 2

---
+++ methods.java
@@ -0,0 +1,17 @@
+public class methods {
+    public static void main(String[] args) {
+      double x = 3.6;
+      double y = 4.5;
+      double z = 2.0;
+      //System.out.println(Math.pow(x, z));
+      //System.out.println(Math.pow(x, Math.pow(y, z)));
+      //System.out.println(Math.abs(y));
+      //System.out.println(Math.sqrt(Math.pow(x*y, z)));
+  
+      System.out.print(Math.pow(x, z) + " " + Math.pow(x, Math.pow(y, z)) + " " + (Math.abs(y)) + " " + Math.sqrt(Math.pow(x*y, z)));
+      
+  
+  
+  
+    }
+  }
